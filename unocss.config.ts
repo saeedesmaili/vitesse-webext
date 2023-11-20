e2a0881fb3eeae92c796ff92aa747a230ff3424e
Saeed Esmaili <saaeed.es20@gmail.com>
2023-11-20 19:51:41 +0100

    add tailwind unocss presets

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'unocss/vite'
-import { presetAttributify, presetIcons, presetUno, transformerDirectives } from 'unocss'
+import { defineConfig } from "unocss/vite";
+import {
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  transformerDirectives,
+} from "unocss";
+import presetWind from "@unocss/preset-wind";
 
 export default defineConfig({
-  presets: [
-    presetUno(),
-    presetAttributify(),
-    presetIcons(),
-  ],
-  transformers: [
-    transformerDirectives(),
-  ],
-})
+  presets: [presetUno(), presetAttributify(), presetIcons(), presetWind()],
+  transformers: [transformerDirectives()],
+});
